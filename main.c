69e3505d1c50222e3d58ad0b62250c7d9140278d
lyseng168 <rt6026010158@camtech.edu.kh>
2026-07-09 17:53:01 +0700

    Move seats into main.c; remove bus_reservation.c; ignore build artifacts

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include "bus_reservation.h"
 
+Passenger seats[ROWS][COLS];
+
+void initializeSeats() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            seats[i][j].booked = 0;
+            seats[i][j].name[0] = '\0';
+            seats[i][j].phone[0] = '\0';
+            seats[i][j].price = 0.0f;
+        }
+    }
+}
+
 int main() {
     int choice;
 
